4343f63c359124ad78e0a92fb5c611404bc4e0aa
Neetu Yadav <neetuwork888@gmail.com>
2026-05-10 20:16:36 +0530

    Fixed LinkedIn links

--- script.js
+++ script.js
@@ -1,4 +1,71 @@
-particlesJS.load("particles-js", "particles.json", function() {
-    console.log("Particles.js loaded!");
-  });
- 
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 90,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    color: {
+      value: ["#00ffcc", "#ffffff", "#0066ff"]
+    },
+    shape: {
+      type: "circle"
+    },
+    opacity: {
+      value: 0.5,
+      random: true
+    },
+    size: {
+      value: 3,
+      random: true
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#00ffcc",
+      opacity: 0.3,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 2,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false
+    }
+  },
+
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "grab"
+      },
+      onclick: {
+        enable: true,
+        mode: "push"
+      },
+      resize: true
+    },
+
+    modes: {
+      grab: {
+        distance: 180,
+        line_linked: {
+          opacity: 1
+        }
+      },
+
+      push: {
+        particles_nb: 4
+      }
+    }
+  },
+
+  retina_detect: true
+});
